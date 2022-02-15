fef328006ee0595666ddfbb3800a79bbc67bcd7d
hryeong66 <jhr110326@gmail.com>
2022-02-16 00:26:44 +0900

    ➕[ADD] 프로젝트 파일 추가

---
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabController.swift
+//  InstargramFirestoreTutorial
+//
+//  Created by 장혜령 on 2022/02/16.
+//
+
+import Foundation
---
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstargramFirestoreTutorial
+//
+//  Created by 장혜령 on 2022/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
