560791f99ec74d5c11206931ab4a73845b81f6da
hryeong66 <jhr110326@gmail.com>
2022-02-17 01:09:48 +0900

    ✅[CHORE] number of item in section 추가

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/FeedController.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/FeedController.swift
@@ -7,39 +7,35 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class FeedController: UICollectionViewController{
     
-    //MARK: LifeCycle
+    //MARK: - LifeCycle
     override func viewDidLoad() {
+        super.viewDidLoad()
         configureUI()
     }
     
-    //MARK: Helper
-    
+    //MARK: - Helper
     func configureUI(){
         collectionView.register(FeedCell.self, forCellWithReuseIdentifier: FeedCell.className)
+        collectionView.backgroundColor = .blue
     }
 }
 
-//MARK: DataSource
+//MARK: - DataSource
 extension FeedController {
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 5
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? FeedCell else {return UICollectionViewCell()}
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeedCell.className, for: indexPath) as? FeedCell else { return UICollectionViewCell() }
         cell.backgroundColor = .red
         return cell
     }
 }
 
-
-//MARK: FlowLayout
-
+//MARK: - FlowLayout
 extension FeedController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 200)
