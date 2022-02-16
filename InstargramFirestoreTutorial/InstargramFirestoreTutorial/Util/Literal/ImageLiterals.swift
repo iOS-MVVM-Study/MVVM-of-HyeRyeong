a835b4f805e31c60d7152caf0d567ce61aca3e71
hryeong66 <jhr110326@gmail.com>
2022-02-17 01:07:58 +0900

    ➕[ADD] extension 함수와 profile image 추가

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/Util/Literal/ImageLiterals.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Util/Literal/ImageLiterals.swift
@@ -5,4 +5,31 @@
 //  Created by 장혜령 on 2022/02/17.
 //
 
-import Foundation
+import UIKit
+
+enum ImageLiterals {
+    //MARK: - icon
+    
+    
+    //MARK: - image
+    static var venom7: UIImage { .load(name: "venom-7") }
+    
+}
+
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            print("Image 로드 문제")
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+    func resize(to size: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return image
+    }
+}
