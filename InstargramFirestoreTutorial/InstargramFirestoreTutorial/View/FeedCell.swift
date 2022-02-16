81b88453f2be72eb2ad05002d1875b1b8f63ab9d
hryeong66 <jhr110326@gmail.com>
2022-02-17 01:11:20 +0900

    ✨[FEAT] profile image, name button 추가

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/FeedCell.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/FeedCell.swift
@@ -9,16 +9,56 @@ import UIKit
 
 class FeedCell: UICollectionViewCell{
     
-    //MARK: Properties
+    //MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        print("FeedCell profileImageView")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = ImageLiterals.venom7
+        return imageView
+    }()
     
-    //MARK: LifeCycle
+    private lazy var userNameButton: UIButton = {
+        let button = UIButton(type: .system)
+        print("FeedCell lazy var userNameButton")
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .bold)
+        button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - LifeCycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        print("FeedCell init")
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func configureUI(){
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,
+                                paddingTop: 12, paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(userNameButton)
+        userNameButton.centerY(inView: profileImageView,
+                               leftAnchor: profileImageView.rightAnchor,
+                               paddingLeft: 8)
+    }
+ 
+    //MARK: - Actions
+    @objc
+    func didTapUserName(){
+        print("DEBUG: didTapUserName")
+    }
+    
 }
