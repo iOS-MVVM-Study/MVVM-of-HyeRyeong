ab113ddd46c298883450e5130da3817bdbb2011b
hryeong66 <jhr110326@gmail.com>
2022-02-17 00:15:36 +0900

    ➕[ADD] FeedCell 파일 및 init 코드 추가

---
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeeCell.swift
+//  InstargramFirestoreTutorial
+//
+//  Created by 장혜령 on 2022/02/17.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell{
+    
+    //MARK: Properties
+    
+    //MARK: LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
