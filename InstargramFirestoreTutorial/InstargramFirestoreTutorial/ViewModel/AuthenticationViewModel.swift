dde09455275602adceec44dc0fbd0649dcaa122b
hryeong66 <jhr110326@gmail.com>
2022-02-23 23:11:52 +0900

    ✨[FEAT] register view model 구현 및 protocol 추가

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
-struct LoginViewModel{
+protocol FormViewModel{
+    func updateForm()
+}
+
+protocol AuthenticationViewModel{
+    var formIsValid: Bool { get }
+    var buttonBackGroundColor: UIColor { get }
+    var buttonTitleColor: UIColor { get }
+}
+
+struct LoginViewModel: AuthenticationViewModel{
     var email: String?
     var password: String?
     
@@ -24,6 +34,22 @@ struct LoginViewModel{
     }
 }
 
-struct RegistrationViewModel{
+struct RegistrationViewModel: AuthenticationViewModel{
+    var email: String?
+    var password: String?
+    var fullName: String?
+    var userName: String?
     
+    var formIsValid: Bool {
+        return email?.isEmpty == false && password?.isEmpty == false
+        && fullName?.isEmpty == false && userName?.isEmpty == false
+    }
+    
+    var buttonBackGroundColor: UIColor{
+        return formIsValid ? .systemPurple : .systemIndigo.withAlphaComponent(0.5)
+    }
+    
+    var buttonTitleColor: UIColor{
+        return formIsValid ? .white : UIColor(white: 1, alpha: 0.67)
+    }
 }
