78160e48c00ff13c98273651512f51ac607fbe12
hryeong66 <jhr110326@gmail.com>
2022-02-23 22:41:30 +0900

    ✨[FEAT] Login View model 구현

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by 장혜령 on 2022/02/23.
 //
 
-import Foundation
+import UIKit
+
+struct LoginViewModel{
+    var email: String?
+    var password: String?
+    
+    var formIsValid: Bool{
+        return email?.isEmpty == false && password?.isEmpty == false
+    }
+    
+    var buttonBackGroundColor: UIColor{
+        return formIsValid ? .systemPurple : .systemIndigo.withAlphaComponent(0.5)
+    }
+    
+    var buttonTitleColor: UIColor{
+        return formIsValid ? .white : UIColor(white: 1, alpha: 0.67)
+    }
+}
+
+struct RegistrationViewModel{
+    
+}
