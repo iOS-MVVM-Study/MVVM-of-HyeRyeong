fd29442d4d4c73ad6961b9c14c25c3bcd8f17abd
hryeong66 <jhr110326@gmail.com>
2022-02-23 22:41:58 +0900

    ✅[CHORE] 로그인 버튼 활성화 로직 view model 이용하여 수정

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/Authentication/LoginController.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -11,6 +11,8 @@ class LoginController: UIViewController{
     
     //MARK: - Properties
     
+    private var viewModel = LoginViewModel()
+    
     private let iconImage: UIImageView = {
         let imageView = UIImageView(image: ImageLiterals.instagramLogoWhite)
         imageView.contentMode = .scaleAspectFit
@@ -33,16 +35,18 @@ class LoginController: UIViewController{
         let button = UIButton(type: .system)
         button.setTitle("Log In", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemIndigo
+        button.backgroundColor = .systemIndigo.withAlphaComponent(0.5)
         button.layer.cornerRadius = 5
         button.setHeight(50) // stack view에 넣을 때 크기
         button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.isEnabled = false
         return button
     }()
     
-    private let dontHaveAccountButton: UIButton = {
+    private lazy var dontHaveAccountButton: UIButton = {
         let button = UIButton(type: .system)
         button.attributedTitle(firstPart: "Don't have an account?  ", secondPart: "Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
         return button
     }()
     
@@ -57,11 +61,35 @@ class LoginController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureNotificationObservers()
     }
+    
+    //MARK: Actions
+    
+    @objc
+    private func handleShowSignUp(){
+        let nextVC = RegistrationController()
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc
+    private func textDidChange(sender: UITextField){
+        if sender == emailTextField{
+            viewModel.email = sender.text
+        }else{
+            viewModel.password = sender.text
+        }
+        
+        loginButton.backgroundColor = viewModel.buttonBackGroundColor
+        loginButton.setTitleColor(viewModel.buttonTitleColor, for: .normal)
+        loginButton.isEnabled = viewModel.formIsValid
+    }
+    
     
     //MARK: Helpers
     
     func configureUI(){
+        configureGradientLayer()
         navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.barStyle = .black
         
@@ -70,7 +98,6 @@ class LoginController: UIViewController{
         iconImage.setDimensions(height: 80, width: 120)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
         
-        configureGradirentBackgroundView()
         configureButtons()
         configureTextField()
     }
@@ -86,17 +113,16 @@ class LoginController: UIViewController{
                          paddingTop: 32, paddingLeft: 32, paddingRight: 32)
     }
     
-    private func configureGradirentBackgroundView(){
-        let gradirent = CAGradientLayer()
-        gradirent.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
-        gradirent.locations = [0,1]
-        view.layer.addSublayer(gradirent)
-        gradirent.frame = view.frame
-    }
-    
     private func configureButtons(){
         view.addSubview(dontHaveAccountButton)
         dontHaveAccountButton.centerX(inView: view)
         dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
+    
+    func configureNotificationObservers(){
+        emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+    
+    
 }
