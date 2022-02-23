932b45da380ae0112c3f5eb7b397f46c0804b792
hryeong66 <jhr110326@gmail.com>
2022-02-23 16:17:02 +0900

    ✅[CHORE] 시작 VC 변경

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/SceneDelegate.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
        
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = MainTabController()
+        //window?.rootViewController = MainTabController()
+        window?.rootViewController = UINavigationController(rootViewController: LoginController())
         window?.makeKeyAndVisible()
     }
 
