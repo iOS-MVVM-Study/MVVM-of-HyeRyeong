319344764039c8fcbf58972ba13794fd86f377fb
hryeong66 <jhr110326@gmail.com>
2022-03-03 21:19:00 +0900

    ✨[FEAT] 회원가입하여 firestore에 저장하는 로직 구현

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/API/AuthService.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/API/AuthService.swift
@@ -5,4 +5,36 @@
 //  Created by 장혜령 on 2022/03/03.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+struct AuthCredentials{
+    let email: String
+    let password: String
+    let fullName: String
+    let userName: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    static func registerUser(withCredential credentials: AuthCredentials,
+                             completion: @escaping(Error?) -> Void){
+        ImageUploader.uploadImage(image: credentials.profileImage){ imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email,
+                                   password: credentials.password){ (result, error) in
+                if let error = error{
+                    print("DEBUG : Falied to registerUser \(error.localizedDescription)")
+                    return
+                }
+                guard let uid = result?.user.uid else { return }
+                let data: [String: Any] = ["email" : credentials.email,
+                                           "fullName": credentials.fullName,
+                                           "profileImageUrl" : imageUrl,
+                                           "uid" : uid,
+                                           "userName" : credentials.userName]
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
+    }
+}
+
