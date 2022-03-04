ea7a04ebc26a124efc95a3bbcff28ac43b3bce10
hryeong66 <jhr110326@gmail.com>
2022-03-04 21:59:12 +0900

    ✨[FEAT] uicollectionViewController 상속받은 후 뷰 구성

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/ProfileController.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Controller/ProfileController.swift
@@ -7,16 +7,65 @@
 
 import UIKit
 
-class ProfileController: UIViewController{
-    //MARK: LifeCycle
+class ProfileController: UICollectionViewController{
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
-        configureUI()
+        configureCollectionView()
     }
     
-    //MARK: Helper
-    
-    func configureUI(){
-        view.backgroundColor = .blue
+    // MARK: - Helper
+ 
+    func configureCollectionView(){
+        collectionView.backgroundColor = .white
+        collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: ProfileCell.className)
+        collectionView.register(ProfileHeader.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: ProfileHeader.className)
     }
 }
 
+// MARK: - UICollectionViewDataSource
+extension ProfileController{
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCell.className, for: indexPath) as? ProfileCell else {return UICollectionViewCell()}
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ProfileHeader.className, for: indexPath) as? ProfileHeader else { return UICollectionReusableView()}
+        return header
+    }
+}
+
+
+// MARK: - UICollectionViewDelegate
+extension ProfileController{
+    
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ProfileController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 2) / 3
+        print("width = \(width)")
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 240)
+    }
+}
