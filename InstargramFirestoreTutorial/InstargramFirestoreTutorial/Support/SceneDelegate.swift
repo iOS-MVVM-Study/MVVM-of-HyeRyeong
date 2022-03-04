385bd29aa9cdd84c123af2fd796707dc12cd7e8e
hryeong66 <jhr110326@gmail.com>
2022-03-04 20:43:12 +0900

    ✅[CHORE] root view controller 변경

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/Support/SceneDelegate.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Support/SceneDelegate.swift
@@ -16,8 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
        
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        //window?.rootViewController = MainTabController()
-        window?.rootViewController = UINavigationController(rootViewController: LoginController())
+        window?.rootViewController = MainTabController()
+        //window?.rootViewController = UINavigationController(rootViewController: LoginController())
         window?.makeKeyAndVisible()
     }
 
