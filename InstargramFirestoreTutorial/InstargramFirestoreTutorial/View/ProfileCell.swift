cfde73a977c3ace9aa32d9c74e993d3c5759d25b
hryeong66 <jhr110326@gmail.com>
2022-03-04 21:59:38 +0900

    ✨[FEAT] CVC로 쓰일 cell ui 구현 완료

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/ProfileCell.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/ProfileCell.swift
@@ -5,4 +5,30 @@
 //  Created by 장혜령 on 2022/03/04.
 //
 
-import Foundation
+import UIKit
+
+class ProfileCell: UICollectionViewCell{
+    
+    // MARK: - Properites
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(image: ImageLiterals.venom7)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    // MARK: - Life cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
