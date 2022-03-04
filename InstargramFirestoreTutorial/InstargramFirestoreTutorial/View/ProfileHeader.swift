29bbf3399478e1c81f29ebf10f8c88d9023f0e6c
hryeong66 <jhr110326@gmail.com>
2022-03-04 22:00:15 +0900

    ✨[FEAT] ui 구현 완료

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/ProfileHeader.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/View/ProfileHeader.swift
@@ -5,4 +5,141 @@
 //  Created by 장혜령 on 2022/03/04.
 //
 
-import Foundation
+import UIKit
+
+class ProfileHeader: UICollectionReusableView{
+ 
+    // MARK: - Properites
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView(image: ImageLiterals.venom7)
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 80 / 2
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Eddie Brock"
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleEditProfileFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var postLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = attributedStatText(value: 5, label: "posts")
+        return label
+    }()
+    
+    private lazy var followersLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = attributedStatText(value: 2, label: "followers")
+        return label
+    }()
+    
+    private lazy var followingLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = attributedStatText(value: 1, label: "following")
+        return label
+    }()
+    
+    let gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(ImageLiterals.icGrid, for: .normal)
+        return button
+    }()
+    
+    let listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(ImageLiterals.icList, for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(ImageLiterals.icRibbon, for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    // MARK: - Life cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI(){
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, right: rightAnchor,
+                                       paddingTop: 14, paddingLeft: 24, paddingRight: 24)
+        
+        let stack = UIStackView(arrangedSubviews: [postLabel, followersLabel, followingLabel])
+        stack.distribution = .fillEqually
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView)
+        stack.anchor(left: profileImageView.rightAnchor, right: rightAnchor,
+                     paddingLeft: 12, paddingRight: 12, height: 50)
+        
+        let topDividerLineView = UIView()
+        topDividerLineView.backgroundColor = .lightGray
+        
+        let bottomDividerLineView = UIView()
+        bottomDividerLineView.backgroundColor = .lightGray
+        
+        let buttonStack = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        buttonStack.distribution = .fillEqually
+        
+        addSubview(buttonStack)
+        addSubview(topDividerLineView)
+        addSubview(bottomDividerLineView)
+        
+        buttonStack.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        topDividerLineView.anchor(top: buttonStack.topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+        bottomDividerLineView.anchor(top: buttonStack.bottomAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+    }
+    
+    // MARK: - Action
+    @objc func handleEditProfileFollowTapped(){
+        print("handleEditProfileFollowTapped")
+    }
+    
+    //MARK: - helper
+    func attributedStatText(value: Int, label: String) -> NSAttributedString{
+        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font : UIFont.systemFont(ofSize: 14, weight: .bold)])
+        attributedText.append(NSAttributedString(string: label, attributes: [.font : UIFont.systemFont(ofSize: 14, weight: .bold), .foregroundColor: UIColor.lightGray]))
+        return attributedText
+    }
+    
+}
