77e67bae15e3077226c107241aa01ae4ba12832f
hryeong66 <jhr110326@gmail.com>
2022-03-16 17:37:27 +0900

    ✨[FEAT] firebase 에서 user 정보 가져오는 함수 추가

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/API/UserService.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/API/UserService.swift
@@ -5,4 +5,17 @@
 //  Created by 장혜령 on 2022/03/16.
 //
 
-import Foundation
+import Firebase
+
+struct UserService {
+    static func fetchUser(completion: @escaping(User) -> Void) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        COLLECTION_USERS.document(uid).getDocument { snapshot , error in
+            guard let dictionary = snapshot?.data() else { return }
+            let user = User(dictionary: dictionary)
+            completion(user)
+        }
+    }
+    
+}
+
