785817b9ba96f82666ce6710dbbee43358386960
hryeong66 <jhr110326@gmail.com>
2022-03-16 17:36:39 +0900

    ✨[FEAT] user model 추가

--- InstargramFirestoreTutorial/InstargramFirestoreTutorial/Model/User.swift
+++ InstargramFirestoreTutorial/InstargramFirestoreTutorial/Model/User.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct User {
+    let email: String
+    let fullName: String
+    let profileImageUrl: String
+    let userName: String
+    let uid: String
+    
+    init(dictionary : [String : Any]) {
+        self.email = dictionary["email"] as? String ?? ""
+        self.fullName = dictionary["fullName"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.userName = dictionary["userName"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+    }
+}
